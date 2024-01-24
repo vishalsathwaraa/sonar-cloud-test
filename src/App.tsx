58f2f47691b6d0854ea957bb9fe0b0d112d6472f
vishalsathwaraa <vishalsathwara999@gmail.com>
2024-01-24 18:04:43 +0530

    add new test branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import SearchInput from "./SearchInput";
-import { RiSearch2Line } from "react-icons/ri";
-import { CgOptions } from "react-icons/cg";
-
-const App = () => {
-  const [searchKeyword, setSearchKeyword] = useState("");
+function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <SearchInput
-        placeholder="Search..."
-        parentClassName="w-72"
-        startIcon={<RiSearch2Line />}
-        endIcon={<CgOptions />}
-        rounded
-        value={searchKeyword}
-        handleChange={(e: any) => setSearchKeyword(e.target.value)}
-        border="sm"
-      />
-    </div>
+    <div className="flex justify-center items-center h-screen">Hello</div>
   );
-};
+}
 
 export default App;
