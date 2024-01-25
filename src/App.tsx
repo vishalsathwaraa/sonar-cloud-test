e0e60cd1eb8166d3e3c0e1890749573844b71a94
vishalsathwaraa <vishalsathwara999@gmail.com>
2024-01-25 13:36:14 +0530

    update congig

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 function App() {
+  const [first, setfirst] = useState('')
   return (
     <div className="flex justify-center items-center h-screen">Hello</div>
   );
