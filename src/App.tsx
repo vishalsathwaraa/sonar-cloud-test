260cc34cfb7252c3bbb804d469e43e2d0b0d4ce6
vishalsathwaraa <vishalsathwara999@gmail.com>
2024-01-25 13:39:32 +0530

    update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 function App() {
-  const [first, setfirst] = useState('')
   return (
     <div className="flex justify-center items-center h-screen">Hello</div>
   );
